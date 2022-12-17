919271b8f8edd31c7d00e9685ca18bc8f861f554
Thuldai <prwshshrm@gmail.com>
2022-12-17 15:19:53 +0200

    First commit.
    Used .env for protected files and added some scripts in main java script.

---
+++ bot.js
@@ -0,0 +1,11 @@
+import Twit from "twit";
+import * as Sutukka from "dotenv";
+
+Sutukka.config();
+
+var T = new Twit({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+});
